1880f4a79d08f7b8ee97982913f7175c15a5cd03
Breaadhead <147561933+Breaadhead@users.noreply.github.com>
2025-04-19 21:30:11 -0600

    Add files via upload

---
+++ UDMAF/index.js
@@ -0,0 +1,12 @@
+const faders = document.querySelectorAll('.fade-in-section');
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target); 
+    }
+  });
+}, {
+  threshold: 0.3
+});
+faders.forEach(fader => observer.observe(fader));
